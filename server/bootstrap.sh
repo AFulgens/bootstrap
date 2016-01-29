98bdb3fd301e7ad98cb70c696b0ce4bc04ec2a99
Daniel Kovacs <dko@zurich.ibm.com>
2016-01-29 15:32:52 +0100

    Add first version of server bootstrap.sh

---
+++ server/bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo apt-get update
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+sudo apt-get install -y git python-pip libpython-dev libffi-dev \
+  libssl-dev mysql libmysqlclient-dev python-dev
