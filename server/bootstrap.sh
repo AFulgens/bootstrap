4e90c7fc980181e6313660ba10f629871e78c67b
Daniel Kovacs <dko@zurich.ibm.com>
2016-01-30 10:31:27 +0100

    Tidy syso

--- server/bootstrap.sh
+++ server/bootstrap.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get -y dist-upgrade
-sudo apt-get -y autoremove
+echo "apt-get update"
+sudo apt-get update > /dev/null
+echo "apt-get dist-upgrade"
+sudo apt-get -y dist-upgrade > /dev/null
+echo "apt-get autoremove"
+sudo apt-get -y autoremove > /dev/null
 
+echo "mysql headless install (password is 'toor')"
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password toor'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password toor'
-sudo apt-get install -y git python-pip libpython-dev libffi-dev \
-  libssl-dev mysql-server libmysqlclient-dev python-dev build-essential
 
+echo "apt-get install git python-pip libpython-dev libffi-dev libssl-dev mysql-server libmysqlclient-dev python-dev build-essential"
+sudo apt-get install -y git python-pip libpython-dev libffi-dev \
+  libssl-dev mysql-server libmysqlclient-dev python-dev \
+  build-essential > /dev/null
+
+echo "see you soon"
 sudo reboot
