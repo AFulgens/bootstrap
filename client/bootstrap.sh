765a1d4994984fc4c13fb2c1e7b4f5e3d7f2b028
Daniel Kovacs <dko@zurich.ibm.com>
2016-01-30 10:14:31 +0100

    Fix mysql install

--- client/bootstrap.sh
+++ client/bootstrap.sh
@@ -5,8 +5,12 @@ sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
 sudo apt-get update
 sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password toor'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password toor'
 sudo apt-get install -y git python-pip libpython-dev libffi-dev \
-  libssl-dev mysql libmysqlclient-dev python-dev build-essential vim
+  libssl-dev mysql-server libmysqlclient-dev python-dev \
+  build-essential vim libevent-dev libncurses-dev
 
 mkdir install
 cd install
--- server/bootstrap.sh
+++ server/bootstrap.sh
@@ -3,6 +3,10 @@
 sudo apt-get update
 sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password toor'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password toor'
 sudo apt-get install -y git python-pip libpython-dev libffi-dev \
-  libssl-dev mysql libmysqlclient-dev python-dev build-essential
+  libssl-dev mysql-server libmysqlclient-dev python-dev build-essential
+
 sudo reboot
